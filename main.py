f0294b251a11acbe95ecbc93d87a220e43b566cc
lbvz <128460178+lbvz@users.noreply.github.com>
2023-06-27 06:41:43 +0000

    three

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"Hello lbvz"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+#query parameter
+@app.get("/raspberry")
+async def read_item(time:datetime = datetime.now(),light: float = 0.0, temperature: float = 0.0):
+    return {
+        "時間":time.strftime("%Y%m%d %H:%M:%S"),
+        "光線":light,
+        "溫度":temperature
+    }
